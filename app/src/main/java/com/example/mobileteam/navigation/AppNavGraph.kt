0b59259183b2da9e6e4e96f1e2c037696d02bb70
hangeol99 <hangeol99@naver.com>
2025-05-23 01:01:04 +0900

    화면 네비게이션 구성 및 아이콘 설정 아이콘은 기본 Default 아이콘 사용 BottomNavigationBar.kt에 내비게이션 기능 구현 및 아이콘 설정

--- app/src/main/java/com/example/mobileteam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/mobileteam/navigation/AppNavGraph.kt
@@ -1,2 +1,31 @@
 package com.example.mobileteam.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.mobileteam.ui.main.MainScreen
+import com.example.mobileteam.ui.main.MainViewModel
+import com.example.mobileteam.ui.search.SearchScreen
+import com.example.mobileteam.ui.userinfo.UserInfoScreen
+
+sealed class Screen(val route: String,val icon: ImageVector) {
+    object Main : Screen("메인 화면", Icons.Default.Home)
+    object Search : Screen("활동 검색", Icons.Default.Search)
+    object UserInfo : Screen("내 정보", Icons.Default.Person)
+}
+
+@Composable
+fun AppNavGraph(navController: NavHostController,mainViewModel: MainViewModel) {
+    NavHost(navController = navController, startDestination = Screen.Main.route) {
+        composable(Screen.Main.route) { MainScreen(mainViewModel) }
+        composable(Screen.Search.route) { SearchScreen() }
+        composable(Screen.UserInfo.route) { UserInfoScreen() }
+    }
+}
